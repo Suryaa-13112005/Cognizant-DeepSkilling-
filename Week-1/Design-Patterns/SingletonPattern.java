7c36bef40f40f2d133183c2159d24b5db5ba879e
Suryaa-13112005 <153260970+Suryaa-13112005@users.noreply.github.com>
2026-06-21 20:46:10 +0530

    Update SingletonPattern.java

--- Week-1/Design-Patterns/SingletonPattern.java
+++ Week-1/Design-Patterns/SingletonPattern.java
@@ -1,1 +1,27 @@
-
+class Singleton {
+    private static Singleton instance;
+    private Singleton() {
+        System.out.println("Singleton Object Created");
+    }
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+    public void display() {
+        System.out.println("Hello from Singleton Class");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+        obj1.display();
+        if (obj1 == obj2) {
+            System.out.println("Both objects are same");
+        } else {
+            System.out.println("Objects are different");
+        }
+    }
+}
