d92d5727a2f615919a950b916884af1cd01f2026
Suryaa-13112005 <153260970+Suryaa-13112005@users.noreply.github.com>
2026-06-30 20:54:08 +0530

    Create surya.java

---
+++ Week-2/folder/surya.java
@@ -0,0 +1,17 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Spring Boot 3 REST API!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Cognizant!";
+    }
+
+}
