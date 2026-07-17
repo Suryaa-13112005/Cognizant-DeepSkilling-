bab1ae3d6772e3fa158ef68e26ffea6142c8c075
Suryaa-13112005 <153260970+Suryaa-13112005@users.noreply.github.com>
2026-07-17 20:23:44 +0530

    Create Account.java

---
+++ Week-4/Creating Microservices for account and loan/Account.java
@@ -0,0 +1,24 @@
+public class Account {
+
+    private int accountId;
+    private String accountHolder;
+    private double balance;
+
+    public Account(int accountId, String accountHolder, double balance) {
+        this.accountId = accountId;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
