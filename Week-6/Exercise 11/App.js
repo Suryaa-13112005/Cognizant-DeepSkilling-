20a541b1031367c046b8739ab2a1ebab3fd4253f
Suryaa-13112005 <153260970+Suryaa-13112005@users.noreply.github.com>
2026-07-27 22:15:46 +0530

    Create App.js

---
+++ Week-6/Exercise 11/App.js
@@ -0,0 +1,22 @@
+import Counter from "./Counter";
+import CurrencyConvertor from "./CurrencyConvertor";
+
+function App() {
+
+  return (
+
+    <div style={{ textAlign: "center", marginTop: "30px" }}>
+
+      <Counter />
+
+      <hr />
+
+      <CurrencyConvertor />
+
+    </div>
+
+  );
+
+}
+
+export default App;
