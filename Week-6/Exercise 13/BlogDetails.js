b5d6b026f12cdf7cc2259fd548aeb158ebd05865
Suryaa-13112005 <153260970+Suryaa-13112005@users.noreply.github.com>
2026-07-27 22:22:33 +0530

    Create BlogDetails.js

---
+++ Week-6/Exercise 13/BlogDetails.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function BlogDetails() {
+
+    const blogs = [
+        { id: 1, title: "Introduction to React", author: "John" },
+        { id: 2, title: "React Hooks", author: "David" },
+        { id: 3, title: "React Router", author: "Smith" }
+    ];
+
+    return (
+
+        <div>
+
+            <h2>Blog Details</h2>
+
+            <ul>
+                {
+                    blogs.map(blog => (
+                        <li key={blog.id}>
+                            {blog.title} - {blog.author}
+                        </li>
+                    ))
+                }
+            </ul>
+
+        </div>
+
+    );
+
+}
+
+export default BlogDetails;
